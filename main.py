13c85941246dc5ddab7aa30b4658c75ba238078f
Stijn <lagers-avondeten-0e@icloud.com>
2025-08-12 21:15:40 +0200

    Add initial implementation of main.py with print and type examples

---
+++ main.py
@@ -0,0 +1,22 @@
+### begin
+# om iets te printen gebruik je print(***)
+
+# print("Hello, World!")      # str printen
+# print(1)                    # int printen
+# print(3.14)                 # float printen
+# print(True)                 # bool printen
+# print([1, 2, 3])            # list printen (een verzameling van variabelen)
+# print(["hallo", True, 3])   # list printen
+
+# hoe weet ik wat voor type iets is?
+# print(type("Hello, World!"))  # type van een string
+# print(type(1))                # type van een integer
+# print(type(3.14))             # type van een float
+# print(type(True))             # type van een boolean
+
+# variabelen toewijzen
+naam = "Samantha" # str
+naam = naam.split('ma') # list
+# heel veel code
+
+print(type(naam))
