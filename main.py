80900c5a10fa4079e187307bde8d1228127bba14
degecremeerdekroket <samanthayychen@hotmail.com>
2025-08-12 22:21:35 +0200

    d

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 ### begin
 # om iets te printen gebruik je print(***)
 
-# print("Hello, World!")      # str printen
+#gebruik "" voor string
+# gebruikt () om functies aan te roepen
+# gebrukt {} geformateerde string
+# gebruik [] lijst of slice
+
+
+
+# print("Hello, World!")      # str printen = tekst
 # print(1)                    # int printen
 # print(3.14)                 # float printen
 # print(True)                 # bool printen
@@ -15,8 +22,26 @@
 # print(type(True))             # type van een boolean
 
 # variabelen toewijzen
-naam = "Samantha" # str
-naam = naam.split('ma') # list
-# heel veel code
+# naam1 = "Samantha" # str
+# # naam = naam.split('ma') # list
+# # # heel veel code
 
-print(type(naam))
+# # print(type(naam))
+
+# naam2 = "Stijn"
+# print(naam1 + " en " +naam2)
+
+# #formatteren is invullen - geformateerde string
+# print(f"{naam2} leert mij Python.")
+
+#lijst maken met []
+#met . open je functies
+namen=["Samantha", "Stijn", "Maggie", "Samia"]
+#totaal aantal waarden in lijst
+#print(len(namen))
+
+#loop door namen - stoppen net voor elke , om te kijken wat er stond
+for naam in namen:
+    #iets doen in loop
+    print(f"{naam} is leuk")
+
